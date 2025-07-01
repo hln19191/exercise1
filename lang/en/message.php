962abc9f484a362a4cc92ff4bd402c9315c0c133
hln19191 <heleneath19@gmail.com>
2025-07-01 14:33:12 +0700

    Add files via upload

---
+++ lang/en/message.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'back_to_dashboard' => 'Back to dashboard',
+    'enter_email' => 'Enter your email and password to sign in!',
+    'enter_email_signup' => 'Enter your email and password to sign up!',
+    'update_personal_information' => 'Update your details to keep your profile up-to-date.',
+    'change_password' => 'Ensure your account is using a long, random password to stay secure.',
+    'warning_account_delete' => 'Once your account is deleted, all of its resources and data
+                    will be permanently deleted. Before deleting your account,
+                    please download any data or information that you wish to
+                    retain.',
+    'confirm' => [
+        'delete_account' => 'Are you sure you want to delete your account?',
+        'sure' => 'Are you sure?',
+        'delete' => 'This :title will be deleted permanently!',
+    ],
+    'success' => [
+        'deleted' => ':title :key is deleted successfully!',
+        'profile_is_updated' => 'Your profile is updated successfully',
+    ],
+    'settings' => [
+        'general' => 'The section of this app is designed to provide you with comprehensive control over its behavior, appearance, and your personal data.'
+    ]
+];
